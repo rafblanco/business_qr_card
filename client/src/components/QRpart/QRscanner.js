0202c4b45957ece3c17bea7b12bf8aa33a63cab4
SiennaS <ssienna48@gmail.com>
2019-03-07 15:12:34 -0500

    camera

--- client/src/components/QRpart/QRscanner.js
+++ client/src/components/QRpart/QRscanner.js
@@ -59,7 +59,7 @@ class QRscanner extends Component {
           delay={this.state.delay}
           onError={this.handleError}
           onScan={this.handleScan}
-          style={{ width: "250px", height: "250px" }}
+          style={{ width: "300px", height: "300px" }}
           showViewFinder={true}
         />)
     } else {
